ce55d248c5fffa0efd7d07711f898d5c77ca0256
Parchello <parchellos@gmail.com>
2023-08-10 19:55:55 +0300

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+
+function createGallery(arr) {
+  return arr
+    .map(
+      ({ preview, description, original }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>`
+    )
+    .join('');
+}
+
+container.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
